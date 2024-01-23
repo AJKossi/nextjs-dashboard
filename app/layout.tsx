ec6047b060e922bd378405cff9e865874780e264
Kossi ADANOU <kjadanou@gmail.com>
2024-01-23 23:19:57 +0000

    added nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Omnisoft Dashboard',
+  description: 'Build Dashbord including customers, invoices, insights with #Postgres and NextJs, Tailwind, Typescript. Using CI/CD with Vercel in deployment ',
+  metadataBase: new URL('https://nextjs-dashboard-liard-chi.vercel.app/'),
+};
 export default function RootLayout({
   children,
 }: {
