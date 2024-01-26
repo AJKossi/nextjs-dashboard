1048b59e50f699d0fd362feedbc61dc8c15a610a
Kossi ADANOU <kjadanou@gmail.com>
2024-01-26 15:47:22 +0000

    updated login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,7 +11,7 @@ import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  const [errorMessage, dispatch] = useFormState(authenticate,"Invalid credentials.");
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
